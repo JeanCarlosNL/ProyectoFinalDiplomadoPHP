129a9221e143443d703e3957be913fa3ac8a8f4e
NorthKing2812 <51000808+NorthKing2812@users.noreply.github.com>
2019-11-13 16:48:12 -0400

    Update autorizado.php

--- administrador/helpers/autorizado.php
+++ administrador/helpers/autorizado.php
@@ -6,13 +6,13 @@ if(isset($_SESSION['Administrador'])){
 
     if($_SESSION['Administrador'] == null){
         $_SESSION['mensajeAutorizacion'] = "No tiene permizo para acceder";
-        header("location: {$layout->directory}inicioSesion.php");
+        header("location:inicioSesion.php");
         exit();
     }
 
 }else{
     $_SESSION['mensajeAutorizacion'] = "No tiene permizo para acceder";
-    header("location: {$layout->directory}inicioSesion.php");
+    header("location:inicioSesion.php");
     exit();
 }
 
